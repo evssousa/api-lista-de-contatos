d7c8942f9cd8a6edc84137bebd07e7186ac9ad66
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-09-17 14:00:07 +0000

    inserção de contatos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,29 @@
 import express from "express"
+import writeFile = require("fs")
+import fs = require("fs")
+
+const dataSource = "./data/lista.txt"
 
 const router = express.Router()
 
-//
+router.post("/contato", (req, res) => {
+    const { nome } = req.body
+
+    if (!nome || nome.length < 2) {
+        return res.json({ error: "Nome precisa ter pelo menos 2 caracteres." })
+    }
+
+    // processamento dos dados
+    let lista: string[] = []
+    try {
+        const data = await fs.readFile(dataSource, { encoding: "utf8" })
+        lista = data.split("\n")
+    } catch(err) { }
+
+    lista.push(nome)
+    await writeFile(dataSource, lista.join("\n"))
+
+    res.status(201).json({ contato: nome })
+})
 
 export default router
